6be5e50adb1df16cb65fec243608419d1d9d82da
Dizzerin <Dizzerin@gmail.com>
2019-02-12 18:07:29 -0800

    Added non-code related resource files Added .gitignore Created Main.cpp and a Note class

---
+++ Coding Files/Music Project/Music Project/Main.cpp
@@ -0,0 +1,17 @@
+/*
+ *	CPTR 142 Group Project (Project 3)
+ *	Date: 2/12/19
+ *	Authors: Caleb nelson
+ *	Authors: Elliott Claus
+ *	Authors: Devin Grabner
+ *	Description: 
+ *
+ */
+
+#include <iostream>
+
+
+int main() {
+	
+	return 0;
+}
